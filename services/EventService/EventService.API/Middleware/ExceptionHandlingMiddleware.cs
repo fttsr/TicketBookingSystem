982a667755ff164b11dc438a2fbbe324d7a658ef
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:25:48 +0700

    Добавлена обработка исключений EventService

---
+++ services/EventService/EventService.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+namespace EventService.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            } 
+            catch (InvalidOperationException ex)
+            {
+                await HandleExceptionAsync(
+                    context,
+                    HttpStatusCode.BadRequest,
+                    ex.Message);
+            }
+            
+        }
+
+        private static async Task HandleExceptionAsync(
+            HttpContext context,
+            HttpStatusCode statusCode,
+            string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            var response = new
+            {
+                statusCode = (int)statusCode,
+                message
+            };
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
