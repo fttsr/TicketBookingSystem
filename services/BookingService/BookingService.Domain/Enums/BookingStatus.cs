3dc9bda5e94fdb457916ab3bfb21f34b954e4e6e
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:51:41 +0700

    Добавлена сущность бронирования и статусы бронирования

---
+++ services/BookingService/BookingService.Domain/Enums/BookingStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingService.Domain.Enums
+{
+    public enum BookingStatus
+    {
+        Pending,
+        Confirmed,
+        Cancelled
+    }
+}
