868145fe866bec33ecddeef76423a38fbf4a3639
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:23:10 +0700

    Реализован прикладной слой мероприятий с сервисами и dto

---
+++ services/EventService/EventService.Application/Interfaces/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using EventService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventService.Application.Interfaces
+{
+    public interface IEventRepository
+    {
+        Task<List<Event>> GetAllAsync();
+        Task<Event?> GetByIdAsync(Guid id);
+        Task AddAsync(Event eventItem);
+        Task DeleteAsync(Event eventItem);
+        Task SaveChangesAsync();
+    }
+}
---
+++ services/EventService/EventService.Application/Interfaces/IEventService.cs
@@ -0,0 +1,18 @@
+﻿using EventService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventService.Application.Interfaces
+{
+    public interface IEventService
+    {
+        Task<List<EventDto>> GetAllAsync();
+        Task<EventDto?> GetByIdAsync(Guid id);
+        Task<EventDto> CreateAsync(CreateAndUpdateEventDto dto);
+        Task<bool> DeleteAsync(Guid id);
+        Task<EventDto?> UpdateAsync(Guid id, CreateAndUpdateEventDto dto);
+        Task<EventDto?> ReserveTicketAsync(Guid id);
+        Task<EventDto?> CancelReservationAsync(Guid id);
+    }
+}
