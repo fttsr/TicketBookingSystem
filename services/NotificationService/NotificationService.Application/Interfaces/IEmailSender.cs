11de087e9c80639d57e52e3143edeae0eb9876e6
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 03:03:28 +0700

    Добавлены контракты уведомлений и отправки писем по эмейлу

---
+++ services/NotificationService/NotificationService.Application/Interfaces/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Application.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendAsync(string email, string subject, string message);
+    }
+}
---
+++ services/NotificationService/NotificationService.Application/Interfaces/INotificationService.cs
@@ -0,0 +1,14 @@
+﻿using NotificationService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendRegistrationSuccessAsync(RegistrationSuccessNotificationDto dto);
+        Task SendBookingCreatedAsync(BookingCreatedNotificationDto dto);
+        Task SendBookingCancelledAsync(BookingCancelledNotificationDto dto);
+    }
+}
