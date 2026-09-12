f33922fe17b53c5e4410f607242a2e0e5c6ebb58
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:24:26 +0700

    Реализована работа с БД в слое инфраструктуры

---
+++ services/EventService/EventService.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,46 @@
+﻿using EventService.Application.Interfaces;
+using EventService.Domain.Entities;
+using EventService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventService.Infrastructure.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly EventDbContext _context;
+
+        public EventRepository(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Event eventItem)
+        {
+            await _context.Events.AddAsync(eventItem);
+        }
+
+        public Task DeleteAsync(Event eventItem)
+        {
+            _context.Events.Remove(eventItem);
+            return Task.CompletedTask;
+        }
+
+        public async Task<List<Event>> GetAllAsync()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        public async Task<Event?> GetByIdAsync(Guid id)
+        {
+            return await _context.Events.FindAsync(id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
