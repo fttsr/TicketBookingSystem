c88e1ca8f31b5e6d4927a26673989c88b156099f
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:38:59 +0700

    Добавлена сущность пользователя

---
+++ services/AuthService/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
