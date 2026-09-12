5d045858938686768eaea526399b77bd23e7b386
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:43:00 +0700

    Реализован контроллер AuthService

---
+++ services/AuthService/AuthService.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using AuthService.Application.DTOs;
+using AuthService.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterAndLoginDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(RegisterAndLoginDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            return Ok(result);
+        }
+    }
+}
