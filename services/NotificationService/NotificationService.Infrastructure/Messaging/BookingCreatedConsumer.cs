e18a59fedf0e82dca024f70eb129c567aef565a0
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 03:04:52 +0700

    Добавлен consumer события создания брони

---
+++ services/NotificationService/NotificationService.Infrastructure/Messaging/BookingCreatedConsumer.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NotificationService.Application.Interfaces;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using TicketBookingSystem.Contracts.Events;
+
+namespace NotificationService.Infrastructure.Messaging
+{
+    public class BookingCreatedConsumer : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public BookingCreatedConsumer(IConfiguration configuration, IServiceScopeFactory scopeFactory)
+        {
+            _configuration = configuration;
+            _scopeFactory = scopeFactory;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMq:Host"],
+                UserName = _configuration["RabbitMq:Username"],
+                Password = _configuration["RabbitMq:Password"],
+            };
+
+            await using var connection = await factory.CreateConnectionAsync();
+
+            await using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(
+                queue: "booking-created",
+                durable: true,
+                exclusive: false,
+                autoDelete: false);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += async (_, args) =>
+            {
+                try
+                {
+                    var json = Encoding.UTF8.GetString(args.Body.ToArray());
+
+                    var message = JsonSerializer.Deserialize<BookingCreatedEvent>(json);
+
+                    if (message is null)
+                        return;
+
+                    using var scope = _scopeFactory.CreateScope();
+
+                    var notificationService = scope.ServiceProvider.GetRequiredService<INotificationService>();
+
+                    await notificationService.SendBookingCreatedAsync(new Application.DTOs.BookingCreatedNotificationDto
+                    {
+                        Email = message.Email,
+                        BookingId = message.BookingId,
+                        EventTitle = message.EventTitle,
+                        EventDate = message.EventDate,
+                        Venue = message.Venue
+                    });
+
+                    await channel.BasicAckAsync(
+                        deliveryTag: args.DeliveryTag,
+                        multiple: false);
+                } 
+                catch (Exception)
+                {
+                    await channel.BasicNackAsync(
+                        deliveryTag: args.DeliveryTag,
+                        multiple: false,
+                        requeue: true);
+                }
+
+            };
+
+            await channel.BasicConsumeAsync(
+                    queue: "booking-created",
+                    autoAck: false,
+                    consumer: consumer);
+
+            await Task.Delay(
+                Timeout.Infinite,
+                stoppingToken);
+        }
+    }
+}
