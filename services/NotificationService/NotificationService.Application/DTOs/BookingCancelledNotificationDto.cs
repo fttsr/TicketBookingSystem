276819880a09cff3d6de35507ad83106149e6cff
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 03:03:04 +0700

    Добавлены dto для уведомлений

---
+++ services/NotificationService/NotificationService.Application/DTOs/BookingCancelledNotificationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Application.DTOs
+{
+    public class BookingCancelledNotificationDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public Guid BookingId { get; set; }
+        public string EventTitle { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public string Venue { get; set; } = string.Empty;
+    }
+}
---
+++ services/NotificationService/NotificationService.Application/DTOs/RegistrationSuccessNotificationDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Application.DTOs
+{
+    public class RegistrationSuccessNotificationDto
+    {
+        public string Email { get; set; } = String.Empty;
+    }
+}
