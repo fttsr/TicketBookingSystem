d5f8bd84038f754b4d85c0338cd5e0a6e3e77275
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 02:42:00 +0700

    Реализована работа с БД в инфраструктурном слое

---
+++ services/AuthService/AuthService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using AuthService.Application.Interfaces;
+using AuthService.Domain.Entities;
+using AuthService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AuthDbContext _context;
+
+        public UserRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
