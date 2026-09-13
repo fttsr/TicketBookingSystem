866ca954f4579ddfbec1d80fe6622b9c47d25bc8
Prosvirkin Nikita <fetteser.niox@gmail.com>
2026-09-13 22:42:26 +0700

    Добавлен контракт кэша redis

---
+++ services/EventService/EventService.Application/Interfaces/IEventCache.cs
@@ -0,0 +1,14 @@
+﻿using EventService.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventService.Application.Interfaces
+{
+    public interface IEventCache
+    {
+        Task<EventDto?> GetAsync(Guid eventId);
+        Task SetAsync(EventDto dto);
+        Task RemoveAsync(Guid eventId);
+    }
+}
